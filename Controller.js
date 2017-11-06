9915985782c50e42577e216f867fa4c612145e8c
Stewart Duffy <duffy.stewart@gmail.com>
2017-11-07 07:23:45 +1300

    Filters directly from response

--- Controller.js
+++ Controller.js
@@ -9,29 +9,28 @@ const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-const processDates = (cities) => {
-
-  return _(cities)
-    .filter(({ time }) => {
-      const isFive = time.indexOf('5:') > 0;
-      const isPm = time.indexOf('p.m') > 0;
-
-      return isFive && isPm;
-    })
-    .map(({ name, time }) => {
-      const cleanTime = time.replace('p.m.', 'pm').replace('a.m.', 'am');
-
-      console.log('cleanTime: ', cleanTime);
-
-      return {
-        name,
-        time,
-        cleanTime,
-        moment: moment(cleanTime, 'ddd h.mm a').format('dddd, MMMM Do YYYY, h:mm:ss a')
-      };
-    })
-    .value();
-};
+// const processDates = (cities) => {
+//   return _(cities)
+//     .filter(({ time }) => {
+//       const isFive = time.indexOf('5:') > 0;
+//       const isPm = time.indexOf('p.m') > 0;
+//
+//       return isFive && isPm;
+//     })
+//     .map(({ name, time }) => {
+//       const cleanTime = time.replace('p.m.', 'pm').replace('a.m.', 'am');
+//
+//       console.log('cleanTime: ', cleanTime);
+//
+//       return {
+//         name,
+//         time,
+//         cleanTime,
+//         moment: moment(cleanTime, 'ddd h.mm a').format('dddd, MMMM Do YYYY, h:mm:ss a')
+//       };
+//     })
+//     .value();
+// };
 
 // RETURNS ALL THE USERS IN THE DATABASE
 router.get('/', function(req, res) {
@@ -44,7 +43,30 @@ router.get('/', function(req, res) {
       //   const responseData = await processDates(cities);
       //   console.log('responseData: ', responseData);
       // })();
-      res.status(200).send(cities);
+      //   const responseData = await processDates(cities);
+
+      res.status(200).send(
+        _(cities)
+          .filter(({ time }) => {
+            const isFive = time.indexOf('5:') > 0;
+            const isPm = time.indexOf('p.m') > 0;
+
+            return isFive && isPm;
+          })
+          .map(({ name, time }) => {
+            const cleanTime = time.replace('p.m.', 'pm').replace('a.m.', 'am');
+
+            console.log('cleanTime: ', cleanTime);
+
+            return {
+              name,
+              time,
+              cleanTime,
+              moment: moment(cleanTime, 'ddd h.mm a').format('dddd, MMMM Do YYYY, h:mm:ss a')
+            };
+          })
+          .value()
+      );
     })
     .catch(error => {
       console.log(error);
