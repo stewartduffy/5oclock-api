b44d3a617640abca2ec5793453cd5f4e80883279
Stewart Duffy <duffy.stewart@gmail.com>
2017-11-07 07:59:26 +1300

    Just filter then

--- Controller.js
+++ Controller.js
@@ -46,26 +46,37 @@ router.get('/', function(req, res) {
       //   const responseData = await processDates(cities);
 
       res.status(200).send(
-        _(cities)
-          .filter(({ time }) => {
+        _.filter(cities, ({ time }) => {
             const isFive = time.indexOf('5:') > 0;
             const isPm = time.indexOf('p.m') > 0;
 
             return isFive && isPm;
           })
-          .map(({ name, time }) => {
-            const cleanTime = time.replace('p.m.', 'pm').replace('a.m.', 'am');
 
-            console.log('cleanTime: ', cleanTime);
 
-            return {
-              name,
-              time,
-              cleanTime,
-              moment: moment(cleanTime, 'ddd h.mm a').format('dddd, MMMM Do YYYY, h:mm:ss a')
-            };
-          })
-          .value()
+
+        // _(cities)
+        //   .filter(({ time }) => {
+        //     const isFive = time.indexOf('5:') > 0;
+        //     const isPm = time.indexOf('p.m') > 0;
+        //
+        //     return isFive && isPm;
+        //   })
+        //   .map(({ name, time }) => {
+        //     const cleanTime = time.replace('p.m.', 'pm').replace('a.m.', 'am');
+        //
+        //     console.log('cleanTime: ', cleanTime);
+        //
+        //     return {
+        //       name,
+        //       time,
+        //       cleanTime,
+        //       moment: moment(cleanTime, 'ddd h.mm a').format('dddd, MMMM Do YYYY, h:mm:ss a')
+        //     };
+        //   })
+        //   .value()
+
+
       );
     })
     .catch(error => {
